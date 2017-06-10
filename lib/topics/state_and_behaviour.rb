6e0b4344de152f0c5948f1c7631ed45b2e4772bc
sontsevolosa <sontsevolosa@gmail.com>
2017-06-10 20:20:31 +0300

    Add attr_accessor for 'Car' class

--- lib/topics/state_and_behaviour.rb
+++ lib/topics/state_and_behaviour.rb
@@ -3,6 +3,8 @@
 # exercise state and behaviour
 module StateAndBehaviour
   class Car
+    attr_accessor(:year, :color, :model)
+
     def initialize(characteristics)
       @year = characteristics[:year]
       @color = characteristics[:color]
