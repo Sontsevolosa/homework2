601e4768ab0a5baee84d35476b06a798bb1ea592
sontsevolosa <sontsevolosa@gmail.com>
2017-06-10 19:41:04 +0300

    Initialize year, color, model for 'Car' class

--- lib/topics/state_and_behaviour.rb
+++ lib/topics/state_and_behaviour.rb
@@ -3,5 +3,10 @@
 # exercise state and behaviour
 module StateAndBehaviour
   class Car
+    def initialize(year, color, model)
+      @year = year
+      @color = color
+      @model = model
+    end
   end
 end
